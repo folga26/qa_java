d33a97ec191a99b6db21b0c548c7fc9dcfefc74e
Olga Fedorova <okun96@yandex.ru>
2024-06-18 15:34:55 +0300

    FelineTest refactored

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -9,26 +9,22 @@ public class FelineTest {
     public void testEatMeat() throws Exception{
         Feline feline = new Feline();
         Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
-        System.out.println(feline.eatMeat());
     }
     @Test
     public void testGetFamily(){
         Feline feline = new Feline ();
         Assert.assertEquals("Кошачьи", feline.getFamily());
-        System.out.println(feline.getFamily());
     }
 
     @Test
     public void testGetKitten(){
         Feline feline = new Feline();
         Assert.assertEquals(1, feline.getKittens());
-        System.out.println(feline.getKittens());
     }
 
     @Test
     public void testGetKittensCount(){
         Feline feline = new Feline();
         Assert.assertEquals(1, feline.getKittens());
-        System.out.println(feline.getKittens());
     }
 }
